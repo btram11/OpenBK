01912488ee063a5f8ae73926bb303bd8c19a049c
nhattpn <nhat.trantpn2212@hcmut.edu.vn>
2024-12-09 02:22:04 +0700

    remove backend

--- backend/src/routes/index.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const courseRoutes = require('./course');
-const userRoutes = require('./user/user.route');
-const router = express.Router();
-
-router.use('/course', courseRoutes);
-router.use('/user', userRoutes)
-
-module.exports = router;
